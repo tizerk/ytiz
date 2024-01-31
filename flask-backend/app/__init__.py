646c580909e06c9dc3761edd2983aa04ef28bd3d
Jay Joo <jayjoo0727@gmail.com>
2024-01-30 23:25:33 -0800

    CORS configuration

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 
 load_dotenv()
@@ -8,5 +9,9 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("secret_key")
+cors = CORS(
+    app,
+    resources={"/api/*": {"origins": ["https://client-testing-a521.up.railway.app/"]}},
+)
 
 from app import routes
