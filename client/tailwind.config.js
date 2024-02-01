e78591d2cb2dbd576831f9bde8fe3366bb707a13
Jay Joo <jayjoo0727@gmail.com>
2024-01-31 18:43:43 -0800

    Tweaks to gitignore and config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class',
 	theme: {
 		fontFamily: {
 			sans: ['ui-sans-serif', 'system-ui'],
