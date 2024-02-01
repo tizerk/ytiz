027d6b2ee09b59ed3f062599799f983ad166b5cd
Jay Joo <jayjoo0727@gmail.com>
2024-01-31 21:48:46 -0800

    Prettier Tailwind plugin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import LinkInput from './components/LinkInput';
-import './App.css';
+import LinkInput from "./components/LinkInput";
+import "./App.css";
 
 function App() {
-	return (
-		<main className="h-screen overflow-x-hidden box-border bg-[#FBF9FB] p-0 m-0 text-2xl font-league flex flex-col justify-center items-center">
-			<h1 className="text-8xl text-center mt-[5rem] mb-[3rem]">YTIZ-MP3</h1>
-			<LinkInput />
-		</main>
-	);
+  return (
+    <main className="m-0 box-border flex h-screen flex-col items-center justify-center overflow-x-hidden bg-[#FBF9FB] p-0 font-league text-2xl">
+      <h1 className="mb-[3rem] mt-[5rem] text-center text-8xl">YTIZ-MP3</h1>
+      <LinkInput />
+    </main>
+  );
 }
 
 export default App;
