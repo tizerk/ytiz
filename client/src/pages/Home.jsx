b8cfd920dfbede70d771b595f81fd78840f0a390
Jay Joo <jayjoo0727@gmail.com>
2024-04-20 22:36:15 -0700

    Disabled user input

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -84,18 +84,18 @@ function Home(props) {
       </motion.h1>
       <div className="bg-red-500 text-center font-bold text-text">
         <h1>WEBSITE IS CURRENTLY DOWN, PLEASE CHECK BACK LATER</h1>
-        <p>
+        <p className="text-base">
           The hosting provider YTiz uses is currently experiencing an upstream
           traffic issue, and YTiz will not be operational until they fix the
           issue. Sorry for the inconvenience.
         </p>
       </div>
-      <LinkInput
+      {/* <LinkInput
         colorTheme={props.colorTheme}
         className="z-10"
         downloadCallback={handleCallback}
         cumulativeDLCount={formattedDLCount}
-      />
+      /> */}
       {!dlFetchError && (
         <div className="absolute bottom-8 select-none text-lg font-bold text-text opacity-50">
           <p>{formattedDLCount} Files Downloaded!</p>
