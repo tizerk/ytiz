4baa8da7fb5a431dfa9cacf1b3d50c37df28e6f8
Jay Joo <jayjoo0727@gmail.com>
2024-05-18 02:42:36 -0700

    Scaling down backend

--- client/src/components/LinkInput.jsx
+++ client/src/components/LinkInput.jsx
@@ -81,7 +81,7 @@ const qualities = [
 ];
 
 function LinkInput(props) {
-  const fetchMachineID = Math.floor(Math.random() * 7);
+  const fetchMachineID = Math.floor(Math.random() * 6);
   const fetchMachineURLS = [
     import.meta.env.VITE_m1,
     import.meta.env.VITE_m2,
@@ -89,7 +89,6 @@ function LinkInput(props) {
     import.meta.env.VITE_m4,
     import.meta.env.VITE_m5,
     import.meta.env.VITE_m6,
-    import.meta.env.VITE_m7,
   ];
   const fetchURL = fetchMachineURLS[fetchMachineID];
   const [link, setLink] = useState("");
