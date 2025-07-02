04ba0f4b442787ba6bf37fcbd2c07b492eca9190
Jay Joo <jayjoo0727@gmail.com>
2025-07-02 12:32:50 -0700

    Updated yt-dlp backend

--- client/src/pages/Changelog.jsx
+++ client/src/pages/Changelog.jsx
@@ -21,6 +21,12 @@ function Changelog() {
       >
         <ScrollArea className="h-full max-h-[400px] w-full max-w-[500px] rounded-xl bg-slate-700 bg-opacity-40 from-violet-800 to-slate-950 p-8 backdrop-blur-sm">
         <Change
+            header="Release 3.4.8 - July 2nd, 2025"
+            features={[
+              "Updated yt-dlp backend"
+            ]}
+          />
+        <Change
             header="Release 3.4.7 - April 27th, 2025"
             features={[
               "Scaled down backend machines"
