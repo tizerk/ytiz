618c8844b6eb31526ead583add5c74e5ae0cf411
Jay Joo <jayjoo@ucsb.edu>
2025-11-26 14:30:33 +0100

    Removed support button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -59,34 +59,6 @@ function App() {
           <Route path="/changelog" element={<Changelog />} />
         </Routes>
       </AnimatePresence>
-      {isDesktop && (
-        <a
-          className="group absolute bottom-32 left-10 flex rounded-3xl bg-slate-900 px-5 py-3 text-base font-semibold text-gray-200 transition-all duration-200 hover:scale-110 hover:bg-slate-700 hover:drop-shadow-small_glow sm:bottom-8"
-          href="https://ko-fi.com/tizerk"
-          target="_blank"
-        >
-          Support Me
-          <img
-            src={Kofi}
-            className="pl-2 transition-all duration-150 group-hover:rotate-12"
-            alt="Ko-fi Icon"
-          />
-        </a>
-      )}
-      {!isDesktop && (
-        <a
-          className="group absolute bottom-14 left-10 flex rounded-3xl bg-slate-900 px-5 py-3 text-base font-semibold text-gray-200 transition-all duration-200 hover:scale-110 hover:bg-slate-700 hover:drop-shadow-small_glow sm:bottom-8"
-          href="https://ko-fi.com/tizerk"
-          target="_blank"
-        >
-          Tip
-          <img
-            src={Kofi}
-            className=" pl-2 transition-all duration-150 group-hover:rotate-12"
-            alt="Ko-fi Icon"
-          />
-        </a>
-      )}
     </motion.main>
   );
 }
